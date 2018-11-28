f8cba0aace78a8a8f3a1b1c08730a13bbd1dfcc5
Adam <szwaba.adam@gmail.com>
2018-11-28 17:09:52 +0100

    works pretty well now

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Card from 'react-bootstrap/lib/Card';
+import './ContentCard.css';
 
 const ContentCard = ({ index, keys, values }) => {
   return (
-    <div key={index} id={index} className="contentCard">
-      {values.map(value => <div>{value}</div>)}
-    </div>
+    <Card key={index} id={index} sm={2}>
+      <Card.Body>
+        <Card.Title> {values[0]}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">
+          {`${keys[1]}: ${values[1]}`}
+        </Card.Subtitle>
+        <Card.Text as="div">
+          <ul className="main">
+            {
+              values.slice(2, values.length - 3).map((value, i) =>
+                Array.isArray(value) ?
+                  <ul key={i}>
+                    {value.map((item, index) =>
+                      <li key={`sublink ${i}, ${index}`}>{item}</li>)}
+                  </ul>
+                  :
+                  <li key={`${values[0]} ${keys[i + 2]}`}>
+                    {` ${keys[i + 2]}: ${value}`}
+                  </li>
+              )
+            }
+          </ul>
+        </Card.Text>
+      </Card.Body>
+    </Card >
   )
 }
 
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import Form from 'react-bootstrap/lib/Form'
+import FormControl from 'react-bootstrap/lib/FormControl'
 
 const SearchBox = ({
   searchchange,
   activeTab
 }) => {
-  return (<div className="searchBar" >
-    <
-      input type='search'
-      placeholder={
-        `search ${activeTab}`
-      }
-      onChange={
-        searchchange
-      }
-    /> </div >
+  const ph = activeTab;
+  return (
+    <Form className="mr-auto" inline>
+      <FormControl type="text"
+        placeholder={ph}
+        className="mr-sm-2"
+        onChange={searchchange}
+      />
+    </Form>
+    // <div className="searchBar" >
+    //   <
+    //     input type='search'
+    //     placeholder={
+    //       `search ${activeTab}`
+    //     }
+    //     onChange={
+    //       searchchange
+    //     }
+    //   /> </div >
   )
 }
 
