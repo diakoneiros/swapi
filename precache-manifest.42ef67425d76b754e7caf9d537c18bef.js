7e8d0bda27c4fa4889b6785eb22f1bb0c22443f2
Adam <szwaba.adam@gmail.com>
2018-11-28 17:30:30 +0100

    Updates

---
+++ precache-manifest.42ef67425d76b754e7caf9d537c18bef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c4baae64c12ef839806",
+    "url": "/swapi/static/css/main.5b481f5d.chunk.css"
+  },
+  {
+    "revision": "1c4baae64c12ef839806",
+    "url": "/swapi/static/js/main.1c4baae6.chunk.js"
+  },
+  {
+    "revision": "b9d7449de0c9fd5e3938",
+    "url": "/swapi/static/js/1.b9d7449d.chunk.js"
+  },
+  {
+    "revision": "5a32f8fe521c9a95db9d",
+    "url": "/swapi/static/js/runtime~main.5a32f8fe.js"
+  },
+  {
+    "revision": "607074c6ebb5db85c4f8ab8edbf2c3c7",
+    "url": "/swapi/index.html"
+  }
+];
